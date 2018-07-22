224b4f3b52bfb052ebc609e74883e366fae4d0f9
anaruhudson <anaruhudson@anarus-mbp.student.uod.otago.ac.nz>
2018-07-22 13:50:33 +1200

    Created Sale domain class. References #1

---
+++ src/main/java/domain/Sale.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.time.LocalDate;
+import java.util.*;
+
+/**
+ *
+ * @author anaruhudson
+ */
+public class Sale {
+    private String saleID;
+    private java.time.LocalDate date;
+    private String status;
+    private Customer customer;
+    private List<SaleItem> saleList = new ArrayList<>();
+
+    public Sale(String saleID, LocalDate date, String status, Customer customer) {
+        this.saleID = saleID;
+        this.date = date;
+        this.status = status;
+        this.customer = customer;
+    }
+
+    public String getSaleID() {
+        return saleID;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setSaleID(String saleID) {
+        this.saleID = saleID;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    
+    public void addItem(SaleItem saleItem){
+        this.saleList.add(saleItem);
+    }
+    /**TODO: add getTotal() method */
+}
